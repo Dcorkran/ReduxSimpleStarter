5fde23832f5b831dda19615a11fe56c017cad73d
Dillon Corkran <dcorkran972@gmail.com>
2017-05-11 15:11:54 -0600

    working through components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAyjg52GGlRAyN26-iSc8pGAwe4RvJF3bU';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />,document.querySelector('.container'))

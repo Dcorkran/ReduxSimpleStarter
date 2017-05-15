258c49f58438f362e8105c4cb5cc5bb5682b062a
Dillon Corkran <dcorkran972@gmail.com>
2017-05-15 15:16:10 -0600

    :sparkles: react tut

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAyjg52GGlRAyN26-iSc8pGAwe4RvJF3bU';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+})
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
+
 
 ReactDOM.render(<App />,document.querySelector('.container'))
